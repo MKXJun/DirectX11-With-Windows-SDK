fbeb18c8e06d070385560f465252aa6115caac74
MKXJun <757919340@qq.com>
2018-05-12 17:19:53 +0800

    添加项目DirectX11 Initialization

---
+++ 01 DirectX11 Initialization/GameApp.cpp
@@ -0,0 +1,39 @@
+#include "GameApp.h"
+
+GameApp::GameApp(HINSTANCE hInstance)
+	: D3DApp(hInstance)
+{
+}
+
+GameApp::~GameApp()
+{
+}
+
+bool GameApp::Init()
+{
+	if (!D3DApp::Init())
+		return false;
+
+	return true;
+}
+
+void GameApp::OnResize()
+{
+	D3DApp::OnResize();
+}
+
+void GameApp::UpdateScene(float dt)
+{
+
+}
+
+void GameApp::DrawScene()
+{
+	assert(md3dImmediateContext);
+	assert(mSwapChain);
+	static float blue[4] = { 0.0f, 0.0f, 1.0f, 1.0f };	// RGBA = (0,0,255,255)
+	md3dImmediateContext->ClearRenderTargetView(mRenderTargetView.Get(), reinterpret_cast<const float*>(&blue));
+	md3dImmediateContext->ClearDepthStencilView(mDepthStencilView.Get(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+	HR(mSwapChain->Present(0, 0));
+}
---
+++ 01 DirectX11 Initialization/GameApp.h
@@ -0,0 +1,17 @@
+#ifndef GAMEAPP_H
+#define GAMEAPP_H
+#include "d3dApp.h"
+class GameApp : public D3DApp
+{
+public:
+	GameApp(HINSTANCE hInstance);
+	~GameApp();
+
+	bool Init();
+	void OnResize();
+	void UpdateScene(float dt);
+	void DrawScene();
+};
+
+
+#endif

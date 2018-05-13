b6f03bf17cfee8397f8f3fab795275e63e219517
MKXJun <757919340@qq.com>
2018-05-13 17:45:28 +0800

    添加项目Rendering a Cube

---
+++ 03 Rendering a Cube/Cube_VS.hlsl
@@ -0,0 +1,1 @@
+#include "Cube.fx"
---
+++ 03 Rendering a Cube/GameApp.cpp
@@ -0,0 +1,248 @@
+#include "GameApp.h"
+#include <filesystem>
+using namespace DirectX;
+using namespace std::experimental;
+using Microsoft::WRL::ComPtr;
+
+const D3D11_INPUT_ELEMENT_DESC GameApp::VertexPosColor::inputLayout[2] = {
+	{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 }
+};
+
+GameApp::GameApp(HINSTANCE hInstance)
+	: D3DApp(hInstance)
+{
+}
+
+GameApp::~GameApp()
+{
+}
+
+bool GameApp::Init()
+{
+	if (!D3DApp::Init())
+		return false;
+
+	if (!InitEffect())
+		return false;
+
+	if (!InitResource())
+		return false;
+
+	return true;
+}
+
+void GameApp::OnResize()
+{
+	D3DApp::OnResize();
+}
+
+void GameApp::UpdateScene(float dt)
+{
+	// 更新固定缓冲区，让立方体转起来
+	static float phi = 0.0f, theta = 0.0f;
+	phi += 0.00003f, theta += 0.00005f;
+	mCBuffer.world = XMMatrixRotationX(phi) * XMMatrixRotationY(theta);
+	md3dImmediateContext->UpdateSubresource(mConstantBuffer.Get(), 0, nullptr, &mCBuffer, 0, 0);
+}
+
+void GameApp::DrawScene()
+{
+	assert(md3dImmediateContext);
+	assert(mSwapChain);
+
+	static float blue[4] = { 0.0f, 0.0f, 0.0f, 1.0f };	// RGBA = (0,0,0,255)
+	md3dImmediateContext->ClearRenderTargetView(mRenderTargetView.Get(), reinterpret_cast<const float*>(&blue));
+	md3dImmediateContext->ClearDepthStencilView(mDepthStencilView.Get(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+	// 将更新好的固定缓冲区绑定到顶点着色器
+	md3dImmediateContext->VSSetConstantBuffers(0, 1, mConstantBuffer.GetAddressOf());
+
+	// 绘制立方体
+	md3dImmediateContext->DrawIndexed(36, 0, 0);
+	HR(mSwapChain->Present(0, 0));
+}
+
+HRESULT GameApp::CompileShaderFromFile(const WCHAR * szFileName, LPCSTR szEntryPoint, LPCSTR szShaderModel, ID3DBlob ** ppBlobOut)
+{
+	HRESULT hr = S_OK;
+
+	DWORD dwShaderFlags = D3DCOMPILE_ENABLE_STRICTNESS;
+#ifdef _DEBUG
+	// 设置 D3DCOMPILE_DEBUG 标志用于获取着色器调试信息。该标志可以提升调试体验，
+	// 但仍然允许着色器进行优化操作
+	dwShaderFlags |= D3DCOMPILE_DEBUG;
+
+	// 在Debug环境下禁用优化以避免出现一些不合理的情况
+	dwShaderFlags |= D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif
+
+	ComPtr<ID3DBlob> errorBlob = nullptr;
+	hr = D3DCompileFromFile(szFileName, nullptr, nullptr, szEntryPoint, szShaderModel,
+		dwShaderFlags, 0, ppBlobOut, errorBlob.GetAddressOf());
+	if (FAILED(hr))
+	{
+		if (errorBlob != nullptr)
+		{
+			OutputDebugStringA(reinterpret_cast<const char*>(errorBlob->GetBufferPointer()));
+		}
+		return hr;
+	}
+
+
+	return S_OK;
+}
+
+
+
+bool GameApp::InitEffect()
+{
+	ComPtr<ID3DBlob> blob;
+
+	// 已经编译好的着色器文件名
+	filesystem::path psoPath = "Cube_PS.cso", vsoPath = "Cube_VS.cso";
+	std::wstring wstr;
+	// 寻找是否有已经编译好的顶点着色器，否则在运行期编译
+	if (filesystem::exists(vsoPath))
+	{
+		wstr = vsoPath.generic_wstring();
+		HR(D3DReadFileToBlob(wstr.c_str(), blob.GetAddressOf()));
+	}
+	else
+	{
+		HR(CompileShaderFromFile(L"Cube.fx", "VS", "vs_4_0", blob.GetAddressOf()));
+	}
+	// 创建顶点着色器
+	HR(md3dDevice->CreateVertexShader(blob->GetBufferPointer(), blob->GetBufferSize(), nullptr, mVertexShader.GetAddressOf()));
+	// 创建并绑定顶点布局
+	HR(md3dDevice->CreateInputLayout(VertexPosColor::inputLayout, ARRAYSIZE(VertexPosColor::inputLayout),
+		blob->GetBufferPointer(), blob->GetBufferSize(), mVertexLayout.GetAddressOf()));
+	md3dImmediateContext->IASetInputLayout(mVertexLayout.Get());
+
+	blob.Reset();
+
+
+	// 寻找是否有已经编译好的像素着色器，否则在运行期编译
+	if (filesystem::exists(psoPath))
+	{
+		wstr = psoPath.generic_wstring();
+		HR(D3DReadFileToBlob(wstr.c_str(), blob.GetAddressOf()));
+	}
+	else
+	{
+		HR(CompileShaderFromFile(L"Cube.fx", "PS", "ps_4_0", blob.GetAddressOf()));
+	}
+	// 创建像素着色器
+	HR(md3dDevice->CreatePixelShader(blob->GetBufferPointer(), blob->GetBufferSize(), nullptr, mPixelShader.GetAddressOf()));
+
+	blob.Reset();
+
+	// 将着色器绑定到渲染管线
+	md3dImmediateContext->VSSetShader(mVertexShader.Get(), nullptr, 0);
+	md3dImmediateContext->PSSetShader(mPixelShader.Get(), nullptr, 0);
+
+	return true;
+}
+
+bool GameApp::InitResource()
+{
+	// ******************
+	// 设置立方体顶点
+	//    5________ 6
+	//    /|      /|
+	//   /_|_____/ |
+	//  1|4|_ _ 2|_|7
+	//   | /     | /
+	//   |/______|/
+	//  0       3
+	VertexPosColor vertices[] =
+	{
+		{ XMFLOAT3(-1.0f, -1.0f, -1.0f), XMFLOAT4(0.0f, 0.0f, 0.0f, 1.0f) },
+		{ XMFLOAT3(-1.0f, 1.0f, -1.0f), XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f) },
+		{ XMFLOAT3(1.0f, 1.0f, -1.0f), XMFLOAT4(1.0f, 1.0f, 0.0f, 1.0f) },
+		{ XMFLOAT3(1.0f, -1.0f, -1.0f), XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f) },
+		{ XMFLOAT3(-1.0f, -1.0f, 1.0f), XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f) },
+		{ XMFLOAT3(-1.0f, 1.0f, 1.0f), XMFLOAT4(1.0f, 0.0f, 1.0f, 1.0f) },
+		{ XMFLOAT3(1.0f, 1.0f, 1.0f), XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f) },
+		{ XMFLOAT3(1.0f, -1.0f, 1.0f), XMFLOAT4(0.0f, 1.0f, 1.0f, 1.0f) }
+	};
+	// 设置顶点缓冲区描述
+	D3D11_BUFFER_DESC vbd;
+	ZeroMemory(&vbd, sizeof(vbd));
+	vbd.Usage = D3D11_USAGE_DEFAULT;
+	vbd.ByteWidth = sizeof vertices;
+	vbd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vbd.CPUAccessFlags = 0;
+	// 新建顶点缓冲区
+	D3D11_SUBRESOURCE_DATA InitData;
+	ZeroMemory(&InitData, sizeof(InitData));
+	InitData.pSysMem = vertices;
+	HR(md3dDevice->CreateBuffer(&vbd, &InitData, mVertexBuffer.GetAddressOf()));
+
+	// 输入装配阶段的顶点缓冲区设置
+	UINT stride = sizeof(VertexPosColor);	// 跨越字节数
+	UINT offset = 0;						// 起始偏移量
+
+	md3dImmediateContext->IASetVertexBuffers(0, 1, mVertexBuffer.GetAddressOf(), &stride, &offset);
+
+	// ******************
+	// 索引数组
+	WORD indices[] = {
+		// 正面
+		0, 1, 2,
+		2, 3, 0,
+		// 左面
+		4, 5, 1,
+		1, 0, 4,
+		// 顶面
+		1, 5, 6,
+		6, 2, 1,
+		// 背面
+		7, 6, 5,
+		5, 4, 7,
+		// 右面
+		3, 2, 6,
+		6, 7, 3,
+		// 底面
+		4, 0, 3,
+		3, 7, 4
+	};
+	// 设置索引缓冲区描述
+	D3D11_BUFFER_DESC ibd;
+	ZeroMemory(&ibd, sizeof(ibd));
+	ibd.Usage = D3D11_USAGE_DEFAULT;
+	ibd.ByteWidth = sizeof indices;
+	ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+	ibd.CPUAccessFlags = 0;
+	// 新建索引缓冲区
+	InitData.pSysMem = indices;
+	HR(md3dDevice->CreateBuffer(&ibd, &InitData, mIndexBuffer.GetAddressOf()));
+	// 输入装配阶段的索引缓冲区设置
+	md3dImmediateContext->IASetIndexBuffer(mIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0);
+
+
+	// ******************
+	// 设置固定缓冲区描述
+	D3D11_BUFFER_DESC cbd;
+	ZeroMemory(&cbd, sizeof(cbd));
+	cbd.Usage = D3D11_USAGE_DEFAULT;
+	cbd.ByteWidth = sizeof(ConstantBuffer);
+	cbd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	cbd.CPUAccessFlags = 0;
+	// 新建固定缓冲区，不使用初始数据
+	HR(md3dDevice->CreateBuffer(&cbd, nullptr, mConstantBuffer.GetAddressOf()));
+
+	// 初始化固定缓冲区的值
+	mCBuffer.world = XMMatrixIdentity();
+	mCBuffer.view = XMMatrixLookAtLH(
+		XMVectorSet(0.0f, 0.0f, -5.0f, 0.0f),
+		XMVectorSet(0.0f, 0.0f, 0.0f, 0.0f),
+		XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f)
+	);
+	mCBuffer.proj = XMMatrixPerspectiveFovLH(XM_PIDIV2, AspectRatio(), 1.0f, 1000.0f);
+
+	// 设置图元类型
+	md3dImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	return true;
+}

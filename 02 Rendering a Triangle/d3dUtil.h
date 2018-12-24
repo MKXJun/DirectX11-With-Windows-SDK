cf30af3907cde1ebd9db1179ddc1ef748861d72e
MKXJun <757919340@qq.com>
2018-12-24 20:12:13 +0800

    删除dxErr库，Windows SDK 8.0以上已内置错误码消息映射
    -删除dxErr库，Windows SDK 8.0以上已内置错误码消息映射
    -清理无用文件

--- 02 Rendering a Triangle/d3dUtil.h
+++ 02 Rendering a Triangle/d3dUtil.h
@@ -16,10 +16,10 @@
 // 在调试输出窗口中输出格式化错误信息，可选的错误窗口弹出(已汉化)
 // [In]strFile			当前文件名，通常传递宏__FILEW__
 // [In]hlslFileName     当前行号，通常传递宏__LINE__
-// [In]hr				函数返回的HRESULT值
+// [In]hr				函数执行出现问题时返回的HRESULT值
 // [In]strMsg			用于帮助调试定位的字符串，通常传递L#x(可能为NULL)
 // [In]bPopMsgBox       如果为TRUE，则弹出一个消息弹窗告知错误信息
-// 返回值: 传入的HRESULT值
+// 返回值: 形参hr
 HRESULT WINAPI DXTraceW(_In_z_ const WCHAR* strFile, _In_ DWORD dwLine, _In_ HRESULT hr, _In_opt_ const WCHAR* strMsg, _In_ bool bPopMsgBox);
 
 

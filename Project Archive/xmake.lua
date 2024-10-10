a849f3a29cf26f088ade787b5c656da8db48fdca
ligo <1569731402@qq.com>
2024-10-10 21:30:22 +0800

    更新xmake

--- Project Archive/xmake.lua
+++ Project Archive/xmake.lua
@@ -1,2 +1,6 @@
-binDir = path.join(os.projectdir(),"bin/Project Archive")
+if is_mode("debug") then
+    binDir = path.join(os.projectdir(),"bin/Debug/Archive")
+else 
+    binDir = path.join(os.projectdir(),"bin/Release/Archive")
+end
 includes("Mouse and Keyboard")
